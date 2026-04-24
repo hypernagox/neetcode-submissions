bf81cfc844dee4aea5c3ce6b00202614d2a55fd0
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-25 08:14:42 +0900

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        vector<int> ans;ans.reserve(2);
+        for(int i=0;i<numbers.size();++i)
+        {
+            const auto val = target - numbers[i];
+            const auto iter = lower_bound(numbers.begin()+i+1, numbers.end(), val);
+            if(iter != numbers.end() && *iter == val)
+            {
+                ans.emplace_back(i+1);
+                ans.emplace_back(iter - numbers.begin() + 1);
+            }
+        }
+        return ans;
+    }
+};
