f1eebaed4e9e244372da7df993c524a091f4aac1
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-24 12:29:19 +0900

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,46 @@
+
+constexpr const int dy[]{-1,0,1,0};
+constexpr const int dx[]{0,1,0,-1};
+
+class Solution {
+public:
+    vector<vector<char>> b;
+    string w;
+    bool visited[6][6]={};
+    bool GO(const int y, const int x, const int depth)
+    {
+        if(depth == w.size() - 1)
+        {
+            return w[depth] == b[y][x];
+        }
+        if(w[depth] != b[y][x])return false;
+        for(int i=0;i<4;++i)
+        {
+            const auto ny = y + dy[i];
+            const auto nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= b.size() || nx >= b.back().size())continue;
+            if(visited[ny][nx])continue;
+            visited[ny][nx]=1;
+            if(GO(ny,nx,depth + 1))return true;
+            visited[ny][nx]=0;
+        }
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        b=board;
+        w=word;
+        for(int i=0;i<board.size();++i)
+        {
+            for(int j=0;j<board.back().size();++j)
+            {
+                visited[i][j]=1;
+                if(GO(i,j,0))
+                {
+                    return true;
+                }
+                 visited[i][j]=0;
+            }
+        }
+        return false;
+    }
+};
