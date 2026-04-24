ae005f5febc3714835a4aacdbe1cfda860795ff6
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-24 17:30:14 +0900

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(std::string s, int k) {
+        int count[26]{};
+        int l = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        for (int r = 0; r < s.size(); ++r) {
+            const int idx = s[r] - 'A';
+            ++count[idx];
+
+            maxFreq = std::max(maxFreq, count[idx]);
+
+            while ((r - l + 1) - maxFreq > k) {
+                --count[s[l] - 'A'];
+                ++l;
+            }
+
+            ans = std::max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+};
