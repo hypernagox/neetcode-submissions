8c3b510782e89ddffc3d80902c71846d8c15b3c4
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-30 10:10:31 +0900

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,43 @@
+#include <cstring>
+class Solution {
+public:
+    string S;
+    int memo[1001][1001];
+    int GO(const int l ,const int r)
+    {
+        if(l >= r)return 1;
+        auto& ref = memo[l][r];
+        if(~ref)return ref;
+        if(S[l] == S[r])
+        {
+            return ref = GO(l + 1, r - 1);
+        }
+        else
+        {
+            return ref = 0;
+        }
+    }
+    string longestPalindrome(string s)
+    {
+        S = s;
+        memset(memo,-1,sizeof(memo));
+        int len = -1;
+        int l,r;
+        for(int i=0;i<s.size();++i)
+        {
+            for(int j=i; j< s.size();++j)
+            {
+                if(GO(i,j))
+                {
+                    if(len < (j - i))
+                    {
+                        l = i;
+                        r = j;
+                        len = (j - i);
+                    }
+                }
+            }
+        }
+        return s.substr(l, len + 1);
+    }
+};
