5ecb0680b3f7b22fb9e5cb4eaf38c1d489f620db
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-05-04 21:49:38 +0900

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) 
+    {
+        unordered_set<int> s;
+        s.reserve(nums.size());
+        for(const auto i : nums)
+        {
+            if(!s.emplace(i).second)return true;
+        }
+        return false;
+    }
+};
